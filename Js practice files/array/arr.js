5398b3acc1992fda409ca6643431cdcc9f87b85c
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-10-08 22:04:06 +0530

    day 9  dsa

---
+++ Js practice files/array/arr.js
@@ -0,0 +1,21 @@
+/**
+Given a sorted array of positive integers and a target value, 
+count the number of pairs (i, j) where i < j and array[i] + array[j] <= target.
+*/
+
+function countAffordablePairs(prices, budget) {
+  // Write your code here
+  let count = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let k = i + 1; k < prices.length; k++) {
+      if (prices[i] + prices[k] <= budget) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(countAffordablePairs([1, 2, 3, 4, 5], 7)); // 8
+//Time complexity: O(n2)
+// Space complexity: O(1)
